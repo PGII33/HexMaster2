0f57fb44942b02264c2e057edb26f88f1db1e7d8
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2026-02-12 03:52:07 +0100

    updt test_terrain.py: - création du squelette de tests

---
+++ tests/test_terrain.py
@@ -0,0 +1,43 @@
+""" Fichier de test pour la classe Terrain """
+
+from random import randint
+from src.terrain import Terrain
+import unittest
+
+class TestTerrain(unittest.TestCase):
+    
+    def test__init__(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_get_entites(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_debut_tour(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_fin_tour(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_nettoyer_entites_mortes(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_get_case_at(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_avant_attaque(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_apres_attaque(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
+    
+    def test_effectuer_attaque(self):
+        """ Pas implémenté """ #TODO: Implémenter
+        self.skipTest("Pas implémenté")
