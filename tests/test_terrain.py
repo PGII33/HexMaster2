22441e065a5c97a7a786d9d36d8b5a5e8e1dbbb8
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2026-02-16 10:33:50 +0100

    updt test_terrain.py: - implémentation de test_get_case_at & test_nettoyer_entites_mortes

--- tests/test_terrain.py
+++ tests/test_terrain.py
@@ -1,43 +1,81 @@
 """ Fichier de test pour la classe Terrain """
 
-from random import randint
-from src.terrain import Terrain
+
+# pylint: disable=missing-function-docstring, missing-class-docstring, C0200
+
 import unittest
+from random import randint
+from tests.utils import MIN_INT, MAX_INT, BOUCLE_TEST
+from src.case import Case
+from src.creature import Creature
+from src.batiment import Batiment
+from src.terrain import Terrain
+
 
 class TestTerrain(unittest.TestCase):
-    
+
     def test__init__(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
-    
+
     def test_get_entites(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
-    
+
     def test_debut_tour(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
-    
+
     def test_fin_tour(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
-    
+
     def test_nettoyer_entites_mortes(self):
-        """ Pas implémenté """ #TODO: Implémenter
-        self.skipTest("Pas implémenté")
-    
+        cr1 = Creature(pv=0, nom="creature1", pos=(0, 0), cout=0, equipe=1, portee=0,
+                      control=0, comp=[], combat=0, demolition=0, degradation=0, mouv=0)
+        cr2 = Creature(pv=10, nom="creature2", pos=(0, 0), cout=0, equipe=1, portee=0,
+                      control=0, comp=[], combat=0, demolition=0, degradation=0, mouv=0)
+        bt1 = Batiment(pv=0, nom="batiment1", pos=(0, 0), cout=0, equipe=1, portee=0,
+                      control=0, comp=[], combat=0, demolition=0, degradation=0,)
+        bt2 = Batiment(pv=10, nom="batiment2", pos=(0, 0), cout=0, equipe=1, portee=0,
+                      control=0, comp=[], combat=0, demolition=0, degradation=0,)
+        case = Case(pv=0, nom="case", pos=(0, 0), cout=0, equipe=1,
+                    control=0, control_max=10, comp=[])
+        case2 = Case(pv=10, nom="case2", pos=(0, 0), cout=0, equipe=1,
+                    control=0, control_max=10, comp=[])
+        terrain = Terrain(entites=[cr1, cr2, bt1, bt2, case, case2])
+        terrain.nettoyer_entites_mortes()
+        self.assertNotIn(cr1, terrain.entites, "Creature morte non supprimée")
+        self.assertIn(cr2, terrain.entites, "Creature vivante supprimée")
+        self.assertNotIn(bt1, terrain.entites, "Batiment mort non supprimé")
+        self.assertIn(bt2, terrain.entites, "Batiment vivant supprimé")
+        self.assertNotIn(case, terrain.entites, "Case morte non supprimée")
+        self.assertIn(case2, terrain.entites, "Case vivante supprimée")
+
     def test_get_case_at(self):
-        """ Pas implémenté """ #TODO: Implémenter
-        self.skipTest("Pas implémenté")
-    
+        for _ in range(BOUCLE_TEST):
+            x = randint(MIN_INT, MAX_INT)
+            y = randint(MIN_INT, MAX_INT)
+            case = Case(pv=0, nom="", pos=(x, y), cout=0, equipe=0,
+                        control=0, control_max=0, comp=[])
+            cases = [Case(pv=0, nom="", pos=(randint(MIN_INT, MAX_INT), randint(MIN_INT, MAX_INT)),
+                          cout=0, equipe=0, control=0, control_max=0, comp=[]) for _ in range(10)]
+            terrain = Terrain(entites=cases + [case])
+            self.assertEqual(terrain.get_case_at((x, y)), case)
+            terrain = Terrain(entites=[case])
+            self.assertEqual(terrain.get_case_at((x, y)), case)
+
+            terrain2 = Terrain(entites=cases)
+            self.assertIsNone(terrain2.get_case_at((x, y)), "get_case_at a trouvé une case inexistante")
+
     def test_avant_attaque(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
-    
+
     def test_apres_attaque(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
-    
+
     def test_effectuer_attaque(self):
-        """ Pas implémenté """ #TODO: Implémenter
+        """ Pas implémenté """  # TODO: Implémenter
         self.skipTest("Pas implémenté")
