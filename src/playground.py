38612d44a2a654328903791dac73fa50d00e3daf
Micky <micky.mari@etu.u-bordeaux.fr>
2026-05-29 22:58:40 +0200

    rename c_temp -> c_debug_cercle

--- src/playground.py
+++ src/playground.py
@@ -41,9 +41,9 @@ def creer_playground()-> Classique:
     c_archere = [chargeur.creer_instance("archere", (1, 1), 4)]
     c_belier = [chargeur.creer_instance("belier", (1, 2), 3)]
 
-    c_temp = [chargeur.creer_instance("cercle", (1, 3), 1)]
+    c_debug_cercle = [chargeur.creer_instance("cercle", (0, 2), 1)]
 
-    c_creature = c_chevalier + c_fermier + c_bucheron + c_archere + c_belier + c_temp
+    c_creature = c_chevalier + c_fermier + c_bucheron + c_archere + c_belier + c_debug_cercle
 
     # Batiment
     c_palissade = [chargeur.creer_instance("palissade", (-1, 0), 2)]
